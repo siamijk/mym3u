42de011e3447943f0af60f271cb418328326afc8
ETC. <133084170+siamijk@users.noreply.github.com>
2024-12-21 07:37:00 +0600

    Add files via upload

---
+++ tv/tsports.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Riyad Sports</title><span style="color: #b45f06;">RiyadSports.Great-Site.Net</span>
+<meta content="never" name="referrer"> <title>Watch Live Sports Online</title><link rel="stylesheet" type="text/css" href="/clapp.css"> <script src="//cdn.jsdelivr.net/npm/clappr@latest/dist/clappr.min.js"></script><script src="//cdn.jsdelivr.net/npm/cdnbye@latest/dist/hlsjs-p2p-engine.min.js"></script><script src="//cdn.jsdelivr.net/npm/cdnbye@latest/dist/clappr-plugin.min.js"></script><script type="text/javascript"src="//cdn.jsdelivr.net/gh/clappr/clappr-level-selector-plugin@latest/dist/level-selector.min.js"></script><div id="player"></div><script disable-devtool-auto src='https://cdn.jsdelivr.net/npm/disable-devtool@latest'></script><script>var player = new Clappr.Player(
+{source: "http://103.96.235.18:8080/sports1/index.m3u8",parentId: "#player",autoPlay: true,height: '100%',width: '100%',plugins: [CDNByeClapprPlugin, LevelSelector],levelSelectorConfig: {labels: {6: '', // 1080kbps
+5: '', // 720kbpsz
+4: '', // 576kbps
+3: '', // 360kbps
+2: '', // 270kbps
+1: '', // 240kbps
+0: '', // 144kbps
+},labelCallback: function(playbackLevel, customLabel) {return customLabel + playbackLevel.level.height+'p'; // High 720p
+}},playback: {hlsjsConfig: {// Other hlsjsConfig options provided by hls.js
+p2pConfig: {logLevel: 'debug',live: true,        // set to true in live mode
+// Other p2pConfig options provided by CDNBye
+}}}});</script>
